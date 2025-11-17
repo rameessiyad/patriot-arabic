9400432350b27705c7e1627f3c616e0fcb767c65
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-11-17 18:28:04 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById("close-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburgerIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("-translate-x-full");
+  hamburgerIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
+
+  document.body.classList.add("overflow-hidden");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("-translate-x-full");
+  closeIcon.classList.add("hidden");
+  hamburgerIcon.classList.remove("hidden");
+
+  document.body.classList.remove("overflow-hidden");
+});
+
+
